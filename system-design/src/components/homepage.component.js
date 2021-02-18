3d2a6a00b08a691e5e7607cf422ca9a552fc65e7
esmail saba <somaasom3a@gmail.com>
2021-02-18 04:23:51 +0200

    lol

--- system-design/src/components/homepage.component.js
+++ system-design/src/components/homepage.component.js
@@ -1,42 +1,40 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Dropdown } from 'react-bootstrap';
-import { Form } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { InputGroup } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
-import {Row} from 'react-bootstrap';
+import { Button, Icon } from 'semantic-ui-react';
 export default class Navbar extends Component {
 
   render() {
     return (
+        <div style = {{width:"100vh"}}>
         <form>
-
-        <h3>Log in</h3>
-
-        <div className="form-group">
-            <label>Email</label>
-            <input type="email" className="form-control" placeholder="Enter email" />
-        </div>
-
-        <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="form-control" placeholder="Enter password" />
-        </div>
-
-        <div className="form-group">
-            <div className="custom-control custom-checkbox">
-                <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-            </div>
-        </div>
-
-        <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
-        <p className="forgot-password text-right">
-            Forgot <a href="#">password?</a>
-        </p>
-    </form>
-    );
+          <div className="form-group" style = {{width:"100vh",position: 'absolute', left: '50%', top: '30%',
+        transform: 'translate(-50%, -50%)'}}> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+               
+                />
+          </div>
+          <div  style = {{width:"100vh",position: 'absolute', left: '50%', top: '50%',
+        transform: 'translate(-50%, -50%)'}} className="form-group"> 
+            <label>Password: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                />
+          </div>
+          <div>
+    <Button  animated  style = {{width:"10vh",position: 'absolute', left: '50%', top: '70%',
+        transform: 'translate(-50%, -50%)'}}>
+      <Button.Content visible>login</Button.Content>
+      <Button.Content hidden>
+        <Icon name='arrow right' />
+      </Button.Content>
+    </Button>
+  </div>
+  
+        </form>
+      </div>
+    )
   }
 }
