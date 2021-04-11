f13a2e38fc5c12e86345e6317796a834f09f5b36
esmail saba <somaasom3a@gmail.com>
2021-04-11 21:51:12 +0200

    hh

--- system-design/backend/server.js
+++ system-design/backend/server.js
@@ -1,46 +1,37 @@
 const express = require('express');
+
 const cors = require('cors');
+
 const mongoose = require('mongoose');
+
 require('dotenv').config();
+
 const app = express();
-app.get('/', (req, res) => { res.send('Hello from Express!')})
-const port = process.env.PORT || 5000
-console.log(port)
-app.use( (req, response, next)=> {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-response.setHeader("Access-Control-Allow-Credentials", "true");
-response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
-  next();
-});
+const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
+
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
+
 console.log(uri)
-mongoose.connect(  uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+
+mongoose.connect("mongodb+srv://lila:lila@cluster0.vtbxz.mongodb.net/General?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true }
+
 );
+
 const connection = mongoose.connection;
+
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully"+" "+connection.toString());
+
+console.log("MongoDB database connection established successfully");
+
 })
 
-const classesRouter = require('./routes/classes');
-const usersRouter = require('./routes/users');
-const facultyRouter = require('./routes/faculty');
-const sectionsRouter = require('./routes/section');
-const adminRouter = require('./routes/admins');
-
-app.use('/classes', classesRouter);
-app.use('/users', usersRouter);
-app.use('/faculty', facultyRouter);
-app.use('/section', sectionsRouter);
-app.use('/admins', adminRouter);
-
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
 
-module.exports = app;
+console.log("Server is running on port: ${port}");
+
+});
